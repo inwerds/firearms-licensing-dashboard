db1962d6ae041e4f2ce10449dd01f23f75a6da60
Your Name <you@example.com>
2026-05-21 19:59:17 -0400

    Stable DuckDB version

--- charts.py
+++ charts.py
@@ -63,5 +63,30 @@ fig3 = px.bar(
     color_continuous_scale="Reds",
 )
 
-
 fig3.show()
+
+# Aggregate by zip
+zip_counts = (
+    df[df["applicant_zip"].notna()]
+    .groupby("applicant_zip")
+    .size()
+    .reset_index(name="applications")
+)
+
+fig4 = px.choropleth(
+    zip_counts,
+    geojson="https://raw.githubusercontent.com/OpenDataDE/State-zip-code-GeoJSON/master/ma_massachusetts_zip_codes_geo.min.json",
+    locations="applicant_zip",
+    featureidkey="properties.ZCTA5CE10",
+    color="applications",
+    color_continuous_scale="Blues",
+    title="Firearms License Applications by Zip Code",
+    labels={"applications": "Total Applications"}
+)
+
+fig4.update_geos(
+    fitbounds="locations",  # zoom to MA automatically
+    visible=False           # hide the rest of the world
+)
+
+fig4.show()
